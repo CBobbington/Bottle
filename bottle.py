ca46216e006549b057adf5d8607cc140cabd699a
Erlind Acob <erlind.acob@gmail.com>
2015-10-31 20:45:18 +0000

    Added GPIO indicators, added querying

--- bottle.py
+++ bottle.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 from zeroconf import Zeroconf, ServiceInfo
 import RPi.GPIO as GPIO
 import netifaces, socket
 import logging, logging.config, sys, os, argparse
 import json, shelve
+import fnmatch
+import RPi.GPIO as GPIO
 import pika
 
 SERVER_NAME = "Team 23's Message Bottle Server"
@@ -10,12 +14,14 @@ VIRTUAL_HOST = "/bottle"
 EXCHANGE_NAME = "pebble"
 ROUTING_KEY = "actions"
 MSG_DB = shelve.open("bottle.msgs", writeback=True)
+GPIO_PINS = [11,12,13,15]
+GPIO_EN = False
 
 COMMAND_DESC = "RabbitMQ-based server for distribution and storage of text messages"
 DEFAULT_LOGFILE = "bottle.log"
 LOG_FMT = logging.Formatter(fmt="%(asctime)s [%(levelname)-8s] %(message)s", datefmt="%b %d %H:%M:%S")
 LOG = logging.getLogger()
-LOG.setLevel(logging.DEBUG)
+LOG.setLevel(logging.INFO)
 
 def getServiceName():
 	return SERVER_NAME + "._http._tcp.local."
@@ -52,31 +58,62 @@ def processCmd(ch, method, properties, body):
 		if "msgs" not in MSG_DB:
 			MSG_DB['msgs'] = []
 		MSG_DB['msgs'].append(msg)
-		sendMsg(msg["Dest"], json.dumps({"Status": "success"})) 
+		sendMsg(msg["Dest"], json.dumps({"Status": "success"}))
+		updatePins()
 	elif msg["Action"].lower() == "pull" or msg["Action"].lower() == "pullr":
 		toReturn = {}
 		if "msgs" not in MSG_DB or len(MSG_DB["msgs"]) == 0:
-			sendMsg(msg["Dest"], json.dumps({"Status": "failed"}))
+			sendMsg(msg["Dest"], json.dumps({"Status": "failed", "Reason": "No messages available"}))
 		else:
-			if msg["Action"] == "pull":
-				LOG.info("PULL request recieved")
-				sendMsg(msg["Dest"], json.dumps(MSG_DB["msgs"].pop(0)))
+			msgList = MSG_DB["msgs"]
+			if "Query_Author" in msg and msg["Query_Author"]:
+				msgList = filter(lambda storedMsg: "Author" in storedMsg and fnMatch.fnMatch(storedMsg["Author"], msg["Query_Author"]), msgList)
+			if "Query_Age" in msg and msg["Query_Age"]:
+				msgList = filter(lambda storedMsg: "Age" in storedMsg and fnMatch.fnMatch(storedMsg["Age"], msg["Query_Age"]), msgList)
+			if "Query_Subject" in msg and msg["Query_Subject"]:
+				msgList = filter(lambda storedMsg: "Subject" in storedMsg and fnMatch.fnMatch(storedMsg["Subject"], msg["Query_Subject"]), msgList)
+			if "Query_Message" in msg and msg["Query_Message"]:
+				msgList = filter(lambda storedMsg: "Message" in storedMsg and fnMatch.fnMatch(storedMsg["Message"], msg["Query_Message"]), msgList)
+
+			if len(msgList == 0):
+				sendMsg(msg["Dest"], json.dumps({"Status": "failed", "Reason": "No messages available"}))
 			else:
-				LOG.info("PULLR request recieved")
-				sendMsg(msg["Dest"], json.dumps(MSG_DB["msgs"][0]))
+				pulledMsg = msgList[0]
+				if msg["Action"].lower() == "pull":
+					LOG.info("PULL request recieved")
+					MSG_DB["msgs"].remove(pulledMsg)
+					updatePins()
+				else:
+					LOG.info("PULLR request recieved")
+				sendMsg(msg["Dest"], json.dumps(pulledMsg))
 	else:
-		LOG.info("Recieved message with unknown command \'%s\'" % (msg["Action"],))
-		sendMsg(msg["Dest"], json.dumps({"Status": "failed"}))
+		LOG.info("Recieved message with unknown command \'%s\'" % (msg["Action"]))
+		sendMsg(msg["Dest"], json.dumps({"Status": "failed", "Reason": "Unknown command \'%s\'"} % (msg["Action"])))
 		
 def sendMsg(dest, msg):
 	LOG.info("Sending message %s to %s" %(msg, dest,))
 	tempConn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', virtual_host=VIRTUAL_HOST))
 	tempChan = tempConn.channel()
-	tempChan.queue_declare(None, queue=str(dest), passive=True)
+	tempChan.queue_declare(queue=str(dest), passive=True)
 	tempChan.basic_publish(exchange="", routing_key=str(dest), body=msg)
 	tempConn.close()
 
-if __name__ == "__main__":	
+def updatePins():
+	numMsgs = len(MSG_DB["msgs"]) if "msgs" in MSG_DB else 0
+	LOG.info("%d messages stored on this server" % numMsgs)
+	if GPIO_EN:
+		for i, pin in enumerate(GPIO_PINS):
+			GPIO.output(pin, numMsgs & 2**i)
+
+if __name__ == "__main__":
+	# Attempt to configure GPIO
+	GPIO.setmode(GPIO.BOARD)
+	try:
+		GPIO.setup(GPIO_PINS, GPIO.OUT)
+		GPIO_EN = True
+	except RuntimeError:
+		pass	
+
 	# Configure logging
 	parser = argparse.ArgumentParser(description=COMMAND_DESC)
 	parser.add_argument("-l", "--enable_fg_logging", action='store_true', help="Enable logging when running in the foreground")
@@ -97,6 +134,10 @@ if __name__ == "__main__":
 		fileHandler.setFormatter(LOG_FMT)
 		LOG.addHandler(fileHandler)
 		LOG.info("Outputting to logfile \'%s\'" % (args.logfile,))
+
+	if not GPIO_EN:
+		LOG.warn("Could not configure GPIO pins, make sure this srunning with superuser priveleges!")
+	updatePins()
 
 	LOG.info("Starting server...")
 
